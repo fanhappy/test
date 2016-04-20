4469b4233a1f37314f3f9d49d26bb69783ef3148
fanzhilin <tarena@joy15.angelaaarons.net>
2016-04-20 11:29:06 +0800

    刚刚创建的新工程。

---
+++ git-test/git-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git-test
+//
+//  Created by fanzhilin on 16/4/20.
+//  Copyright © 2016年 樊志林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git-test/git-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git-test
+//
+//  Created by fanzhilin on 16/4/20.
+//  Copyright © 2016年 樊志林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
